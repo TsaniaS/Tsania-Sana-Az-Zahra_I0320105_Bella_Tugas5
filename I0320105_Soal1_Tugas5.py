83e0619d48c7ae1e4f28dd75f175db81a660cc4a
Tsania Sana <tsania.sana@gmail.com>
2021-04-02 11:18:44 -0800

    Tugas 5

---
+++ I0320105_Soal1_Tugas5.py
@@ -0,0 +1,8 @@
+# Soal Nomer 1
+Nama = input("Masukkan nama anda: ")
+JenisKelamin = input("Masukkan jenis kelamin: ")
+if (JenisKelamin == 'Perempuan'):
+    print('Selamat datang' + ',' + ' ' + 'Nyonya' + ' ' + str(Nama) + '!')
+else:
+    print('Selamat datang' + ',' + ' ' + 'Tuan' + ' ' + str(Nama) + '!')
+print()
